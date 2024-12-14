25481442e7aaabad04a1553ae5c705335b15384a
Uro Lyi <urolyi1@gmail.com>
2024-12-14 16:04:00 -0500

    Fix various bugs, clean-up, and add more test

--- skyjo_immutable.py
+++ skyjo_immutable.py
@@ -40,8 +40,8 @@ class SkyjoActionType(enum.Enum):
 @dataclasses.dataclass(slots=True, frozen=True)
 class SkyjoAction:
     action_type: SkyjoActionType = dataclasses.field()
-    row_idx: int | None = dataclasses.field(default=None)
-    col_idx: int | None = dataclasses.field(default=None)
+    row_idx: int | None = None
+    col_idx: int | None = None
 
 
 @dataclasses.dataclass(slots=True, frozen=True)
@@ -69,6 +69,55 @@ class Hand:
             (i // NUM_COLUMNS, i % NUM_COLUMNS)
             for i in np.where((self.is_cleared == 0) & (self.is_flipped == 1))[0]
         ]
+
+    @property
+    def valid_discard_and_flip_actions(self) -> list[SkyjoAction]:
+        actions = [
+            SkyjoAction(SkyjoActionType.DISCARD_AND_FLIP, row_idx, col_idx)
+            for row_idx, col_idx in self.face_down_indices
+        ]
+        return actions
+
+    @property
+    def valid_place_from_discard_actions(self) -> list[SkyjoAction]:
+        actions = [
+            SkyjoAction(SkyjoActionType.PLACE_FROM_DISCARD, row_idx, col_idx)
+            for row_idx, col_idx in self.visible_indices + self.face_down_indices
+        ]
+        return actions
+
+    @property
+    def valid_place_drawn_actions(self) -> list[SkyjoAction]:
+        actions = [
+            SkyjoAction(SkyjoActionType.PLACE_DRAWN, row_idx, col_idx)
+            for row_idx, col_idx in self.visible_indices + self.face_down_indices
+        ]
+        return actions
+
+    @staticmethod
+    def clearable(cards: np.array, is_flipped: np.array, col_idx: int):
+        """This is a static method so we can check whether the updated Hand
+        we are about to create needs to have any cleared columns, preventing us
+        from creating any intermediate Hands with incorrect is_cleared values."""
+        all_same = True
+        for r in range(NUM_ROWS):
+            all_same = (
+                all_same
+                and is_flipped[Hand.flat_index(r, col_idx)]
+                and cards[Hand.flat_index(0, col_idx)]
+                == cards[Hand.flat_index(r, col_idx)]
+            )
+        return all_same
+
+    def has_all_revealed(self) -> bool:
+        # To be cleared the is_flipped value would also be set to 1, so we can just
+        # count how many flipped cards there are without worrying about cleared counts.
+        return self.is_flipped.sum() == NUM_COLUMNS * NUM_ROWS
+
+    def flat_index(row_idx: int, col_idx: int) -> int:
+        assert row_idx < NUM_ROWS and row_idx >= 0, "row index out of range"
+        assert col_idx < NUM_COLUMNS and col_idx >= 0, "col index out of range"
+        return row_idx * NUM_COLUMNS + col_idx
 
     def copy(self) -> Self:
         return Hand(
@@ -109,60 +158,29 @@ class Hand:
 
         return replaced_card, Hand(is_flipped=flipped, is_cleared=cleared, cards=cards)
 
-    def flat_index(row_idx: int, col_idx: int) -> int:
-        assert row_idx < NUM_ROWS and row_idx >= 0, "row index out of range"
-        assert col_idx < NUM_COLUMNS and col_idx >= 0, "col index out of range"
-        return row_idx * NUM_COLUMNS + col_idx
-
-    @staticmethod
-    def clearable(cards: np.array, is_flipped: np.array, col_idx: int):
-        all_same = True
-        for r in range(NUM_ROWS):
-            all_same = (
-                all_same
-                and is_flipped[Hand.flat_index(r, col_idx)]
-                and cards[Hand.flat_index(0, col_idx)]
-                == cards[Hand.flat_index(r, col_idx)]
-            )
-        return all_same
-
-    def initial_flips(self) -> Self:
-        assert (
-            len(self.face_down_indices) == NUM_COLUMNS * NUM_ROWS
-        ), "Cannot run initial_flips() when there are already flipped cards"
-        return self.flip(0, 0).flip(0, 1)
-
     def visible_points(self) -> int:
         return self.cards[
             np.where((self.is_cleared == 0) & (self.is_flipped == 1))
         ].sum()
 
-    def has_all_revealed(self) -> bool:
-        return self.is_flipped.sum() == NUM_COLUMNS * NUM_ROWS
-
     def total_points(self) -> int:
-        return self.cards[np.where((self.is_cleared == 0))].sum()
+        """Returns the value of the hand after revealing all cards.
+        This includes any previously undiscovered clearing columns"""
+        assert (
+            self.is_cleared.sum() % NUM_ROWS == 0
+        ), f"somehow cleared cards not a multiple of {NUM_ROWS}"
+        revealed_hand = self.copy()
+        while len(revealed_hand.face_down_indices) > 0:
+            revealed_hand = revealed_hand.flip(*revealed_hand.face_down_indices[0])
+        return revealed_hand.cards[np.where(revealed_hand.is_cleared == 0)].sum()
 
-    def valid_discard_and_flip_actions(self) -> list[SkyjoAction]:
-        actions = [
-            SkyjoAction(SkyjoActionType.DISCARD_AND_FLIP, row_idx, col_idx)
-            for row_idx, col_idx in self.face_down_indices
-        ]
-        return actions
-
-    def valid_place_from_discard_actions(self) -> list[SkyjoAction]:
-        actions = [
-            SkyjoAction(SkyjoActionType.PLACE_FROM_DISCARD, row_idx, col_idx)
-            for row_idx, col_idx in self.visible_indices + self.face_down_indices
-        ]
-        return actions
-
-    def valid_place_drawn_actions(self) -> list[SkyjoAction]:
-        actions = [
-            SkyjoAction(SkyjoActionType.PLACE_DRAWN, row_idx, col_idx)
-            for row_idx, col_idx in self.visible_indices + self.face_down_indices
-        ]
-        return actions
+    def initial_flips(self) -> Self:
+        """This is just a temporary solution.
+        So that we don't have to additionally code initial flip action/decisions"""
+        assert (
+            len(self.face_down_indices) == NUM_COLUMNS * NUM_ROWS
+        ), "Cannot run initial_flips() when there are already flipped cards"
+        return self.flip(0, 0).flip(0, 1)
 
     def _render(self, xray: bool = False) -> list[str]:
         card_width = max(max(len(str(card)) for card in self.cards), 2)
@@ -260,9 +278,9 @@ class DiscardPile:
 
 @dataclasses.dataclass(slots=True, frozen=True)
 class ImmutableSkyjoState:
-    num_players: int = dataclasses.field()
+    num_players: int
     """Number of players in the game"""
-    player_scores: np.array = dataclasses.field()
+    player_scores: np.array
     """Current player scores"""
     hands: list[Hand] = dataclasses.field(default=None)
     """The list of participating players."""
@@ -287,18 +305,38 @@ class ImmutableSkyjoState:
     winning_player: int | None = None
     """Index of player who won the game"""
 
+    @staticmethod
+    def compute_round_scores(
+        hands: list[Hand], round_ending_player_idx: int
+    ) -> np.array:
+        round_scores = np.zeros(len(hands))
+        for player_idx in range(len(hands)):
+            round_scores[player_idx] += hands[player_idx].total_points()
+        if (
+            round_scores[round_ending_player_idx]
+            >= np.concat(
+                [
+                    round_scores[:round_ending_player_idx],
+                    round_scores[round_ending_player_idx + 1 :],
+                ],
+            ).min()
+        ):
+            round_scores[round_ending_player_idx] *= 2
+        return round_scores
+
     def setup_round(self) -> Self:
         next_deck = Deck(_cards=DECK, _rng=self.deck._rng).shuffle()
         next_hands, next_deck = next_deck.deal_hands(self.num_players)
         top_card, next_deck = next_deck.draw_top_card()
         next_hands = [hand.initial_flips() for hand in next_hands]
         starting_player = self.round_ending_player
+        # if first round of game set starting player to player with highest showing value
         if self.round_ending_player is None:
             hand_values = [hand.visible_points() for hand in next_hands]
             starting_player = hand_values.index(max(hand_values))
         next_valid_actions = next_hands[
             starting_player
-        ].valid_place_from_discard_actions() + [SkyjoAction(SkyjoActionType.DRAW)]
+        ].valid_place_from_discard_actions + [SkyjoAction(SkyjoActionType.DRAW)]
         next_state = dataclasses.replace(
             self,
             player_scores=self.player_scores.copy(),
@@ -316,128 +354,85 @@ class ImmutableSkyjoState:
         return next_state
 
     def take_action(self, action: SkyjoAction) -> Self:
+        next_player_scores = self.player_scores.copy()
+        next_hands = [hand.copy() for hand in self.hands]
+        next_deck = self.deck.copy()
+        next_drawn_card = None
+        next_round_count = self.round_count
+        next_turn_count = self.turn_count + 1
+        next_player = (self.curr_player + 1) % self.num_players
+
         if action.action_type == SkyjoActionType.DRAW:
-            drawn_card, next_deck = self.deck.draw_top_card()
+            next_discard_pile = self.discard_pile.copy()
+            next_drawn_card, next_deck = self.deck.draw_top_card()
+            next_turn_count = self.turn_count
+            next_player = self.curr_player
             next_valid_actions = (
-                self.hands[self.curr_player].valid_discard_and_flip_actions()
-                + self.hands[self.curr_player].valid_place_drawn_actions()
+                self.hands[self.curr_player].valid_discard_and_flip_actions
+                + self.hands[self.curr_player].valid_place_drawn_actions
             )
-            next_state = dataclasses.replace(
-                self,
-                player_scores=self.player_scores.copy(),
-                hands=[hand.copy() for hand in self.hands],
-                deck=next_deck,
-                discard_pile=self.discard_pile.copy(),
-                drawn_card=drawn_card,
-                curr_player=self.curr_player,
-                turn_count=self.turn_count,
-                round_count=self.round_count,
-                is_round_ending=self.is_round_ending,
-                round_ending_player=self.round_ending_player,
-                valid_actions=next_valid_actions,
-            )
+
         elif action.action_type == SkyjoActionType.PLACE_DRAWN:
-            next_player = (self.curr_player + 1) % self.num_players
-            next_hands = [hand.copy() for hand in self.hands]
             replaced_card, next_hand = next_hands[self.curr_player].replace(
                 action.row_idx, action.col_idx, self.drawn_card
             )
             next_hands[self.curr_player] = next_hand
             next_discard_pile = self.discard_pile.discard(replaced_card)
-            next_is_round_ending = next_hands[self.curr_player].has_all_revealed()
-            round_ending_player = None
-            if next_is_round_ending:
-                round_ending_player = self.curr_player
             next_valid_actions = next_hands[
                 next_player
-            ].valid_place_from_discard_actions() + [SkyjoAction(SkyjoActionType.DRAW)]
+            ].valid_place_from_discard_actions + [SkyjoAction(SkyjoActionType.DRAW)]
 
-            next_state = dataclasses.replace(
-                self,
-                player_scores=self.player_scores.copy(),
-                hands=next_hands,
-                deck=self.deck.copy(),
-                discard_pile=next_discard_pile,
-                drawn_card=None,
-                curr_player=(self.curr_player + 1) % self.num_players,
-                turn_count=self.turn_count + 1,
-                round_count=self.round_count,
-                is_round_ending=(self.is_round_ending or next_is_round_ending),
-                round_ending_player=(self.round_ending_player or round_ending_player),
-                valid_actions=next_valid_actions,
-            )
         elif action.action_type == SkyjoActionType.DISCARD_AND_FLIP:
             next_discard_pile = self.discard_pile.discard(self.drawn_card)
-            next_player = (self.curr_player + 1) % self.num_players
-            next_hands = [hand.copy() for hand in self.hands]
             next_hands[self.curr_player] = next_hands[self.curr_player].flip(
                 action.row_idx, action.col_idx
             )
             next_valid_actions = next_hands[
                 next_player
-            ].valid_place_from_discard_actions() + [SkyjoAction(SkyjoActionType.DRAW)]
-            next_is_round_ending = next_hands[self.curr_player].has_all_revealed()
-            round_ending_player = None
-            if next_is_round_ending:
-                round_ending_player = self.curr_player
-            next_state = dataclasses.replace(
-                self,
-                player_scores=self.player_scores.copy(),
-                hands=next_hands,
-                deck=self.deck.copy(),
-                discard_pile=next_discard_pile,
-                drawn_card=None,
-                curr_player=next_player,
-                turn_count=self.turn_count + 1,
-                round_count=self.round_count,
-                is_round_ending=(self.is_round_ending or next_is_round_ending),
-                round_ending_player=self.round_ending_player or round_ending_player,
-                valid_actions=next_valid_actions,
-            )
+            ].valid_place_from_discard_actions + [SkyjoAction(SkyjoActionType.DRAW)]
 
         elif action.action_type == SkyjoActionType.PLACE_FROM_DISCARD:
-            next_player = (self.curr_player + 1) % self.num_players
-            next_hands = [hand.copy() for hand in self.hands]
             replaced_card, next_hand = next_hands[self.curr_player].replace(
                 action.row_idx, action.col_idx, self.discard_pile.top_card
             )
             next_hands[self.curr_player] = next_hand
             next_discard_pile = self.discard_pile.replace_top_card(replaced_card)
-            next_is_round_ending = next_hands[self.curr_player].has_all_revealed()
-            round_ending_player = None
-            if next_is_round_ending:
-                round_ending_player = self.curr_player
             next_valid_actions = next_hands[
                 next_player
-            ].valid_place_from_discard_actions() + [SkyjoAction(SkyjoActionType.DRAW)]
-            next_state = dataclasses.replace(
-                self,
-                player_scores=self.player_scores.copy(),
-                hands=next_hands,
-                deck=self.deck.copy(),
-                discard_pile=next_discard_pile,
-                drawn_card=None,
-                curr_player=(self.curr_player + 1) % self.num_players,
-                turn_count=self.turn_count + 1,
-                round_count=self.round_count,
-                is_round_ending=(self.is_round_ending or next_is_round_ending),
-                round_ending_player=self.round_ending_player or round_ending_player,
-                valid_actions=next_valid_actions,
-            )
+            ].valid_place_from_discard_actions + [SkyjoAction(SkyjoActionType.DRAW)]
+
         else:
             raise ValueError("This is not a supported action type")
+
+        has_all_revealed = next_hands[self.curr_player].has_all_revealed()
+        next_round_ending_player = self.round_ending_player
+        if has_all_revealed and not self.is_round_ending:
+            next_round_ending_player = self.curr_player
+
+        next_state = dataclasses.replace(
+            self,
+            player_scores=next_player_scores,
+            hands=next_hands,
+            deck=next_deck,
+            discard_pile=next_discard_pile,
+            drawn_card=next_drawn_card,
+            curr_player=next_player,
+            turn_count=next_turn_count,
+            round_count=next_round_count,
+            is_round_ending=(self.is_round_ending or has_all_revealed),
+            round_ending_player=next_round_ending_player,
+            valid_actions=next_valid_actions,
+        )
 
         if (
             next_state.is_round_ending
             and next_state.curr_player == next_state.round_ending_player
         ):
-            next_player_scores = next_state.player_scores.copy()
-            for player_idx in range(next_state.num_players):
-                next_player_scores[player_idx] += next_state.hands[
-                    player_idx
-                ].total_points()
+            round_scores = ImmutableSkyjoState.compute_round_scores(
+                next_state.hands, next_state.round_ending_player
+            )
             next_state = dataclasses.replace(
-                next_state, player_scores=next_player_scores
+                next_state, player_scores=(next_state.player_scores + round_scores)
             ).setup_round()
             if max(next_state.player_scores) >= 100:
                 next_state = dataclasses.replace(
@@ -459,7 +454,9 @@ class ImmutableSkyjoState:
             f" drawn card: {self.drawn_card}"
             f"  round: {self.round_count}"
             f"  turn: {self.turn_count}"
+            f" player_scores: {self.player_scores}"
             + ("  (ending)" if self.is_round_ending else "")
+            + (f"  ({self.round_ending_player})" if self.is_round_ending else "")
         )
 
         hands_render = [""]
@@ -494,20 +491,6 @@ class RandomPlayer:
         self.name = name
 
     def decide_action(self, state: ImmutableSkyjoState) -> SkyjoAction:
-        if len(state.valid_actions) == 1:
-            return state.valid_actions[0]
+        if len(state.valid_actions) == 0:
+            raise RuntimeError("no valid actions")
         return state.valid_actions[random.randint(0, len(state.valid_actions) - 1)]
-
-
-if __name__ == "__main__":
-    winner_counts = [0, 0]
-    for _ in range(10000):
-        players = [RandomPlayer("a"), RandomPlayer("b")]
-        state = ImmutableSkyjoState(
-            num_players=2, player_scores=np.zeros(2), deck=Deck(_cards=DECK)
-        ).setup_round()
-
-        while state.winning_player is None:
-            state = state.take_action(players[state.curr_player].decide_action(state))
-        winner_counts[state.winning_player] += 1
-    print(winner_counts)
