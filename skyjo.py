3f3dc636ef72fab4edebb66a50aa041ded89eaaf
Noah Kim <noahbkim@gmail.com>
2025-04-06 14:58:29 -0400

    Unify action mask

--- skyjo.py
+++ skyjo.py
@@ -36,16 +36,33 @@ ROW_COUNT = 3
 COLUMN_COUNT = 4
 FINGER_COUNT = ROW_COUNT * COLUMN_COUNT
 
+ACTION_FLIP_SECOND = 0  # Revealing cards in hand
+ACTION_DRAW_OR_TAKE = 1  # Start of turn, whether to draw or take discard
+ACTION_FLIP_OR_REPLACE = 2  # After draw, whether to discard and flip or replace
+ACTION_REPLACE = 3  # After take discard, which card to replace
+ACTION_SIZE = 4
+
+MASK_FLIP_SECOND_BELOW = 0
+MASK_FLIP_SECOND_RIGHT = 1
+MASK_DRAW = 2
+MASK_TAKE = 3
+MASK_FLIP = 4
+MASK_REPLACE = MASK_FLIP + 12
+MASK_SIZE = MASK_REPLACE + 12
+
 GAME_TOP = 0
 GAME_DISCARDS = GAME_TOP + CARD_SIZE
-GAME_SCORES = GAME_DISCARDS + CARD_SIZE
+GAME_ACTION = GAME_DISCARDS + CARD_SIZE
+GAME_SCORES = GAME_ACTION + ACTION_SIZE
 GAME_SIZE = GAME_SCORES + PLAYER_COUNT
+
 
 assert CARD_SIZE == CARD_P12 + 1
 assert FINGER_COUNT == 12
 assert GAME_DISCARDS == 15
-assert GAME_SCORES == 30
-assert GAME_SIZE == 38
+assert GAME_ACTION == 30
+assert GAME_SCORES == 34
+assert GAME_SIZE == 42
 
 Game: TypeAlias = np.ndarray[tuple[int], np.uint8]
 """Top discard/drawn card, count of discards, player scores."""
@@ -56,7 +73,7 @@ Table: TypeAlias = np.ndarray[tuple[int, int, int, int], np.uint8]
 Deck: TypeAlias = np.ndarray[tuple[int], np.uint8]
 """Cards remaining in the deck in lieu of a random seed."""
 
-Skyjo: TypeAlias = tuple[Game, Table, Deck, int]
+Skyjo: TypeAlias = tuple[Game, Table, Deck, int, int]
 """A lightweight representation of a Skyjo game."""
 
 
@@ -124,7 +141,7 @@ def _rotate_table(table: Table, players: int) -> None:
     """Copy `table`, rotating all hands left by one."""
 
     swap = table[0].copy()
-    for i in range(players):
+    for i in range(players - 1):
         table[i] = table[i + 1]
     table[players - 1] = swap
 
@@ -168,12 +185,13 @@ def new(players: int) -> Skyjo:
     """Generate a fresh game with no visible discard."""
 
     game = np.ndarray((GAME_SIZE,), dtype=np.uint8)
+    game[GAME_ACTION + ACTION_FLIP_SECOND] = 1
     table_shape = (PLAYER_COUNT, ROW_COUNT, COLUMN_COUNT, FINGER_SIZE)
     table = np.ndarray(table_shape, dtype=np.uint8, order="C")
     table[:players, :, :, FINGER_HIDDEN] = 1
     deck = np.ndarray((CARD_SIZE,), dtype=np.uint8)
     deck[:] = CARD_COUNTS
-    return game, table, deck, players
+    return game, table, deck, players, 0
 
 
 # MARK: Convenience
@@ -252,76 +270,6 @@ def get_score(skyjo: Skyjo, player: int = 0) -> int:
     return score
 
 
-# MARK: Branching
-
-
-def get_second_flip_mask(skyjo: Skyjo) -> np.ndarray[tuple[int], np.uint8]:
-    """Compute the space of options for a second flip.
-
-    At the start of the game before a discard is dealt the (0, 0) card
-    is flipped for every player in a vacuum (they cannot observe each
-    other). They are then given the choice of whether to flip the card
-    in the same column, [1, 0], or in the same row [0, 1]. Therefore,
-    this mask is a constant [1, 1].
-    """
-
-    return np.array([1, 1], dtype=np.uint8)  # type: ignore
-
-
-def get_draw_or_take_mask(skyjo: Skyjo) -> np.ndarray[tuple[int], np.uint8]:
-    """The space of options for turn actions.
-
-    At the start of each turn, players must either draw a card from the
-    pile, [1, 0], or take the discard, [0, 1]. Therefore, this mask is
-    a constant [1, 1].
-    """
-
-    return np.array([1, 1], dtype=np.uint8)  # type: ignore
-
-
-def get_flip_or_replace_mask_for_draw(
-    skyjo: Skyjo,
-) -> np.ndarray[tuple[int, int, int], np.uint8]:
-    """The space of options if the user draws a card.
-
-    If the player draws a card, they must either discard it and flip
-    a card in their hand, [0, row, column], or replace a card in their
-    hand with it, discarding what it replaced, [1, row, column].
-    """
-
-    table = skyjo[1]
-
-    result = np.ndarray((2, 3, 3), dtype=np.uint8)
-    for row in range(ROW_COUNT):
-        for column in range(COLUMN_COUNT):
-            if table[0, row, column, FINGER_HIDDEN]:
-                result[:, row, column] = 1  # You both flip or replace
-            elif not table[0, row, column, FINGER_CLEARED]:
-                result[1, row, column] = 1  # You can only replace
-
-    return result
-
-
-def get_flip_or_replace_mask_for_take(
-    skyjo: Skyjo,
-) -> np.ndarray[tuple[int, int, int], np.uint8]:
-    """The space of options if the user takes the discard.
-
-    If the player takes the discard, they must replace a card in their
-    hand with it, discarding what it replaced, [1, row, column].
-    """
-
-    table = skyjo[1]
-
-    result = np.ndarray((2, 3, 3), dtype=np.uint8)
-    for row in range(ROW_COUNT):
-        for column in range(COLUMN_COUNT):
-            if not table[0, row, column, FINGER_CLEARED]:
-                result[1, row, column] = 1  # You can only replace
-
-    return result
-
-
 # MARK: Actions
 
 
@@ -332,6 +280,7 @@ def deal(skyjo: Skyjo, card: int | None = None, rng: Random = random) -> Skyjo:
     table = skyjo[1]
     deck = skyjo[2]
     players = skyjo[3]
+    player = skyjo[4]
 
     assert _get_top(game) is None
 
@@ -345,7 +294,7 @@ def deal(skyjo: Skyjo, card: int | None = None, rng: Random = random) -> Skyjo:
 
     assert top is None
 
-    return new_game, table, new_deck, players
+    return new_game, table, new_deck, players, player
 
 
 def draw(skyjo: Skyjo, card: int | None = None, rng: Random = random) -> Skyjo:
@@ -359,6 +308,7 @@ def draw(skyjo: Skyjo, card: int | None = None, rng: Random = random) -> Skyjo:
     table = skyjo[1]
     deck = skyjo[2]
     players = skyjo[3]
+    player = skyjo[4]
 
     # If no card was provided, randomly pick one.
     if card is None:
@@ -369,7 +319,7 @@ def draw(skyjo: Skyjo, card: int | None = None, rng: Random = random) -> Skyjo:
     new_deck = deck.copy()
     _remove_card(new_deck, card)
 
-    return new_game, table, new_deck, players
+    return new_game, table, new_deck, players, player
 
 
 def take(skyjo: Skyjo) -> Skyjo:
@@ -401,6 +351,7 @@ def flip(
     table = skyjo[1]
     deck = skyjo[2]
     players = skyjo[3]
+    player = skyjo[4]
 
     # Make sure this is a valid move
     if not table[0, row, column, FINGER_HIDDEN]:
@@ -430,7 +381,7 @@ def flip(
     new_deck = deck.copy()
     _remove_card(new_deck, card)
 
-    return new_game, new_table, new_deck, players
+    return new_game, new_table, new_deck, players, player + 1
 
 
 def replace(
@@ -452,6 +403,7 @@ def replace(
     table = skyjo[1]
     deck = skyjo[2]
     players = skyjo[3]
+    player = skyjo[4]
 
     # If the finger is currently hidden, we need to draw, but only if
     # `card` is not specified.
@@ -489,4 +441,58 @@ def replace(
     new_deck = deck.copy()
     _remove_card(new_deck, card)
 
-    return new_game, new_table, new_deck, players
+    return new_game, new_table, new_deck, players, player + 1
+
+
+# MARK: Learning
+
+
+def actions(skyjo: Skyjo) -> np.ndarray[tuple[int], np.uint8]:
+    """Generate a mask of possible actions from the current state.
+
+    The mask is a flat, one-dimensional array where each index
+    corresponds to a valid action:
+
+      - [0, 2) are for the start of the game when players reveal the
+        first two cards in their board. The simulation automatically
+        flips the top left card, then the model is asked to flip
+        either the card immediately below (index 0) or immediately
+        to the right (index 1).
+      - [2, 4) are for the start of each turn, during which players
+        must decide to either draw a card (index 2) or take the last
+        discard (index 3).
+      - [4, 16) represent discarding the drawn card and flipping any
+        currently-hidden one in hand. The row and column are computed
+        by `divmod(index - 4, COLUMN_COUNT)`. This action is only
+        allowed if the player drew a card in the previous decision.
+      - [16, 28) represent replacing a card in hand with the one drawn
+        or taken from the discard. The row and column are computed by
+        `divmod(index - 16, ROW_COUNT)`.
+    """
+
+    game = skyjo[0]
+    table = skyjo[1]
+
+    mask = np.ndarray((MASK_SIZE,), dtype=np.uint8)
+
+    if game[ACTION_FLIP_SECOND]:
+        mask[MASK_FLIP_SECOND_BELOW] = 1
+        mask[MASK_FLIP_SECOND_RIGHT] = 1
+    elif game[ACTION_DRAW_OR_TAKE]:
+        mask[MASK_DRAW] = 1
+        mask[MASK_TAKE] = 1
+    elif game[ACTION_FLIP_OR_REPLACE]:
+        for i in range(FINGER_COUNT):
+            row, column = divmod(i, COLUMN_COUNT)
+            if table[0, row, column, FINGER_HIDDEN]:
+                mask[MASK_FLIP + i] = 1  # You both flip or replace
+                mask[MASK_REPLACE + i] = 1
+            elif not table[0, row, column, FINGER_CLEARED]:
+                mask[MASK_REPLACE + i] = 1  # You can only replace
+    elif game[ACTION_REPLACE]:
+        for i in range(FINGER_COUNT):
+            row, column = divmod(i, COLUMN_COUNT)
+            if not table[0, row, column, FINGER_CLEARED]:
+                mask[MASK_REPLACE + i] = 1  # You can only replace
+
+    return mask
